3170cc59394039ae64ec00fd55a7e20af1b56a07
stephenshen1993 <15810813994@163.com>
2024-08-27 07:55:51 +0800

    init

---
+++ src/main/java/com/stephenshen/arkquant/ArkQuantApplication.java
@@ -0,0 +1,13 @@
+package com.stephenshen.arkquant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkQuantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArkQuantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stephenshen/arkquant/ArkQuantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stephenshen.arkquant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkQuantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
